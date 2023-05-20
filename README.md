c6b96fd6b5b7f60fcd42a76e72073f7732ec8aad
chintan <chintan1612199@gmail.com>
2023-05-20 21:05:03 +0530

    setup project make model and make sign up api .

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # node_Netflix_Movie
 we create api for Netflix Movie 
+# npm install
+
---
+++ index.js
@@ -0,0 +1,23 @@
+const express =require("express")
+const app =express()
+const dotenv = require('dotenv')
+const authRoute =require("./routes/auth")
+
+const mongoose =require("mongoose")
+
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO_URL,{
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+     }).then(()=>console.log("DB connected Successfull !")).catch((err)=>console.log("Errorr Connect Db",err));
+
+app.use(express.json());
+
+
+app.use("/api/auth",authRoute)
+
+app.listen(5000,()=>{
+     console.log("Server is running on 5000 ! Port");
+})
