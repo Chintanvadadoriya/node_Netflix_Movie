c3688e34c91fe32ef74ca3b5d8127bd55f6f8ce3
chintan <chintan1612199@gmail.com>
2023-05-21 14:50:30 +0530

    make list api

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv')
 const authRoute =require("./routes/auth")
 const usersRoute =require("./routes/users")
 const movieRoute =require("./routes/movie")
+const listRoute =require("./routes/list")
+
 
 
 
@@ -23,6 +25,8 @@ app.use(express.json());
 app.use("/api/auth",authRoute)
 app.use("/api/users",usersRoute)
 app.use("/api/movies",movieRoute)
+app.use("/api/lists",listRoute)
+
 
 
 app.listen(5000,()=>{
